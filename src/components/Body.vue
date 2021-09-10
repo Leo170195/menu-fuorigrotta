bf9279a780382291de5ce1c3d818e29d4210ca39
Leonardo <decandia.leonardo95@gmail.com>
2021-09-10 18:27:18 +0200

    fix

--- src/components/Body.vue
+++ src/components/Body.vue
@@ -1,27 +1,27 @@
 <template>
   <div class="text-white p-3">
       <h1 class="text-center mb-5">Menu Fuorigrotta <i class="fas fa-beer"></i></h1>
-    <div class="container">
+    <div class="container pb-5">
         <div class="row">
             <div class="col-12">
                 <h2 id="stuzzicheria" class="mb-3 text-center">Stuzzicheria veloce</h2>
-                <div class="row">
+                <div class="row py-3">
                     <div class="col-9">Tortillas con salsa piccante</div>
                     <div class="col-3 text-md-end">€ 3.00</div>
                 </div>
-                <div class="row">
+                <div class="row pb-3">
                     <div class="col-9">Arachidi</div>
                     <div class="col-3 text-md-end">€ 2.00</div>
                 </div>
-                <div class="row">
+                <div class="row pb-3">
                     <div class="col-9">Rice</div>
                     <div class="col-3 text-md-end">€ 2.00</div>
                 </div>
-                <div class="row">
+                <div class="row pb-3">
                     <div class="col-9">Olive</div>
                     <div class="col-3 text-md-end">€ 3.00</div>
                 </div>
-                <div class="row">
+                <div class="row pb-3">
                     <div class="col-9">Pistacchi</div>
                     <div class="col-3 text-md-end">€ 3.00</div>
                 </div>
@@ -29,79 +29,79 @@
         </div>
     </div>
 
-    <div class="container">
+    <div class="container pb-5">
         <div class="row">
             <div class="col-12">
                 <h2 id="antipasti" class="mb-3 text-center">Antipasti</h2>
-                <div class="row">
+                <div class="row py-3">
                     <div class="col-9">Caprese 2.0</div>
                     <div class="col-3 text-md-end">€ 6.00</div>
                 </div>
-                <div class="row">
+                <div class="row pb-3">
                     <div class="col-9">Carpaccio di salmone affumicato con crostini</div>
                     <div class="col-3 text-md-end">€ 8.00</div>
                 </div>
-                <div class="row">
+                <div class="row pb-3">
                     <div class="col-9">Insalata di polpo</div>
                     <div class="col-3 text-md-end">€ 8.00</div>
                 </div>
-                <div class="row">
+                <div class="row pb-3">
                     <div class="col-9">Caciocavallo alla griglia</div>
                     <div class="col-3 text-md-end">€ 4.00</div>
                 </div>
-                <div class="row">
+                <div class="row pb-3">
                     <div class="col-9">Mortadella alla brace con pistacchi e aceto balsamico</div>
                     <div class="col-3 text-md-end">€ 4.00</div>
                 </div>
-                <div class="row">
+                <div class="row pb-3">
                     <div class="col-9">Crostini crudo e mozzarella</div>
                     <div class="col-3 text-md-end">€ 4.50</div>
                 </div>
-                <div class="row">
+                <div class="row pb-3">
                     <div class="col-9">Crostini spek e scamorza</div>
                     <div class="col-3 text-md-end">€ 4.50</div>
                 </div>
-                <div class="row">
+                <div class="row pb-3">
                     <div class="col-9">Crostini brie e noci</div>
                     <div class="col-3 text-md-end">€ 4.50</div>
                 </div>
-                <div class="row">
+                <div class="row pb-3">
                     <div class="col-9">Crostini misti della casa</div>
                     <div class="col-3 text-md-end">€ 5.00</div>
                 </div>
-                <div class="row">
+                <div class="row pb-3">
                     <div class="col-9">Crostini stracchino e salsiccia macinata</div>
                     <div class="col-3 text-md-end">€ 5.00</div>
                 </div>
-                <div class="row">
+                <div class="row pb-3">
                     <div class="col-9">Crostini stracciatella e salmone</div>
                     <div class="col-3 text-md-end">€ 6.00</div>
                 </div>
-                <div class="row">
+                <div class="row pb-3">
                     <div class="col-9">Crostini stracciatella e pomodori secchi</div>
                     <div class="col-3 text-md-end">€ 6.00</div>
                 </div>
-                <div class="row">
+                <div class="row pb-3">
                     <div class="col-9">Crostini porchetta e crema di carciofi</div>
                     <div class="col-3 text-md-end">€ 6.00</div>
                 </div>
-                <div class="row">
+                <div class="row pb-3">
                     <div class="col-9">Tagliere misto di salumi (x 2 persone)</div>
                     <div class="col-3 text-md-end">€ 8.00</div>
                 </div>
-                <div class="row">
+                <div class="row pb-3">
                     <div class="col-9">Tagliere misto di formaggi (x 2 persone)</div>
                     <div class="col-3 text-md-end">€ 8.00</div>
                 </div>
-                <div class="row">
+                <div class="row pb-3">
                     <div class="col-9">Tagliere misto di salumi e formaggi con marmellate</div>
                     <div class="col-3 text-md-end">€ 15.00</div>
                 </div>
-                <div class="row">
+                <div class="row pb-3">
                     <div class="col-9">Mozzarella di bufala e prosciutto crudo</div>
                     <div class="col-3 text-md-end">€ 9.00</div>
                 </div>
-                <div class="row">
+                <div class="row pb-3">
                     <div class="col-9">Verdure grigliate</div>
                     <div class="col-3 text-md-end">€ 4.00</div>
                 </div>
@@ -109,15 +109,15 @@
         </div>
     </div>
 
-    <div class="container">
+    <div class="container pb-5">
         <div class="row">
             <div class="col-12">
                 <h2 id="insalata" class="mb-3 text-center">Insalata</h2>
-                <div class="row">
+                <div class="row py-3">
                     <div class="col-9">Insalata tricolore (insalata iceberg, cubetti di mozzarella e julienne di pomodori)</div>
                     <div class="col-3 text-md-end">€ 6.00</div>
                 </div>
-                <div class="row">
+                <div class="row pb-3">
                     <div class="col-9">Insalata sfiziosa (insalata iceberg, cubetti di mozzarella e julienne di pomodori, mais, olive nere, tonno, ravanello)</div>
                     <div class="col-3 text-md-end">€ 8.00</div>
                 </div>
@@ -125,27 +125,27 @@
         </div>
     </div>
 
-    <div class="container">
+    <div class="container pb-5">
         <div class="row">
             <div class="col-12">
                 <h2 id="fritti" class="mb-3 text-center">Fritti</h2>
-                <div class="row">
+                <div class="row py-3">
                     <div class="col-9">Patatine fritte</div>
                     <div class="col-3 text-md-end">€ 3.50</div>
                 </div>
-                <div class="row">
+                <div class="row pb-3">
                     <div class="col-9">Frittura di calamari (x 2 persone, su prenotazione)</div>
                     <div class="col-3 text-md-end">€ 13.00</div>
                 </div>
-                <div class="row">
+                <div class="row pb-3">
                     <div class="col-9">Montanare napoletane (4 pz)</div>
                     <div class="col-3 text-md-end">€ 6.00</div>
                 </div>
-                <div class="row">
+                <div class="row pb-3">
                     <div class="col-9">Alette di pollo fritto (6 pz)</div>
                     <div class="col-3 text-md-end">€ 6.00</div>
                 </div>
-                <div class="row">
+                <div class="row pb-3">
                     <div class="col-9">Pettole con sartascinello</div>
                     <div class="col-3 text-md-end">€ 5.00</div>
                 </div>
@@ -153,31 +153,31 @@
         </div>
     </div>
 
-    <div class="container">
+    <div class="container pb-5">
         <div class="row">
             <div class="col-12">
                 <h2 id="terrine" class="mb-3 text-center">Terrine</h2>
-                <div class="row">
+                <div class="row py-3">
                     <div class="col-9">Patate al forno</div>
                     <div class="col-3 text-md-end">€ 3.50</div>
                 </div>
-                <div class="row">
+                <div class="row pb-3">
                     <div class="col-9">Zucchine scamorza e pancetta</div>
                     <div class="col-3 text-md-end">€ 4.50</div>
                 </div>
-                <div class="row">
+                <div class="row pb-3">
                     <div class="col-9">Parmigiana di melanzane</div>
                     <div class="col-3 text-md-end">€ 5.00</div>
                 </div>
-                <div class="row">
+                <div class="row pb-3">
                     <div class="col-9">Gnocchetti alla sorrentina con provola di bufala</div>
                     <div class="col-3 text-md-end">€ 5.00</div>
                 </div>
-                <div class="row">
+                <div class="row pb-3">
                     <div class="col-9">Involtini spek e scamorza</div>
                     <div class="col-3 text-md-end">€ 3.50</div>
                 </div>
-                <div class="row">
+                <div class="row pb-3">
                     <div class="col-9">Polpette al sugo con crostini</div>
                     <div class="col-3 text-md-end">€ 5.00</div>
                 </div>
@@ -185,39 +185,39 @@
         </div>
     </div>
 
-    <div class="container">
+    <div class="container pb-5">
         <div class="row">
             <div class="col-12">
                 <h2 id="carne" class="mb-3 text-center">Carne</h2>
-                <div class="row">
+                <div class="row py-3">
                     <div class="col-9">Tagliata di manzo rucola e grana</div>
                     <div class="col-3 text-md-end">€ 15.00</div>
                 </div>
-                <div class="row">
+                <div class="row pb-3">
                     <div class="col-9">Arrosticini su letto di rucola o verdure di stagione (6 pz)</div>
                     <div class="col-3 text-md-end">€ 6.00</div>
                 </div>
-                <div class="row">
+                <div class="row pb-3">
                     <div class="col-9">Arrosticini con pancetta su letto di rucola o verdure di stagione (6 pz)</div>
                     <div class="col-3 text-md-end">€ 6.00</div>
                 </div>
-                <div class="row">
+                <div class="row pb-3">
                     <div class="col-9">Hamburger con patatine</div>
                     <div class="col-3 text-md-end">€ 5.50</div>
                 </div>
-                <div class="row">
+                <div class="row pb-3">
                     <div class="col-9">Salsiccia di Norcia</div>
                     <div class="col-3 text-md-end">€ 5.50</div>
                 </div>
-                <div class="row">
+                <div class="row pb-3">
                     <div class="col-9">Grigliata mista</div>
                     <div class="col-3 text-md-end">€ 13.00</div>
                 </div>
-                <div class="row">
+                <div class="row pb-3">
                     <div class="col-9">Tagliata di pollo alla thailandese</div>
                     <div class="col-3 text-md-end">€ 7.00</div>
                 </div>
-                <div class="row">
+                <div class="row pb-3">
                     <div class="col-9">Cotoletta con patatine fritte</div>
                     <div class="col-3 text-md-end">€ 4.50</div>
                 </div>
@@ -225,60 +225,60 @@
         </div>
     </div>
 
-    <div class="container">
+    <div class="container pb-5">
         <div class="row">
             <div class="col-12">
                 <h2 id="insalata" class="mb-3 text-center">Insalata</h2>
-                <div class="row">
-                    <div class="col-9">1. Polpo stracciatella e pomodori secchi</div>
+                <div class="row py-3">
+                    <div class="col-9">1 - Polpo stracciatella e pomodori secchi</div>
                     <div class="col-3 text-md-end">€ 6.50</div>
                 </div>
-                <div class="row">
-                    <div class="col-9">2. Straccetti di manzo melanzane e mozzarella</div>
+                <div class="row pb-3">
+                    <div class="col-9">2 - Straccetti di manzo melanzane e mozzarella</div>
                     <div class="col-3 text-md-end">€ 5.50</div>
                 </div>
-                <div class="row">
-                    <div class="col-9">3. Prosciutto crudo mozzarella e rucola</div>
+                <div class="row pb-3">
+                    <div class="col-9">3 - Prosciutto crudo mozzarella e rucola</div>
                     <div class="col-3 text-md-end">€ 4.00</div>
                 </div>
-                <div class="row">
-                    <div class="col-9">4. Parmigiana con provola di bufala</div>
+                <div class="row pb-3">
+                    <div class="col-9">4 - Parmigiana con provola di bufala</div>
                     <div class="col-3 text-md-end">€ 5.50</div>
                 </div>
-                <div class="row">
-                    <div class="col-9">5. Salsiccia di norcia pomodoro e insalata</div>
+                <div class="row pb-3">
+                    <div class="col-9">5 - Salsiccia di norcia pomodoro e insalata</div>
                     <div class="col-3 text-md-end">€ 4.50</div>
                 </div>
-                <div class="row">
-                    <div class="col-9">6. Cotoletta pomodoro e insalata</div>
+                <div class="row pb-3">
+                    <div class="col-9">6 - Cotoletta pomodoro e insalata</div>
                     <div class="col-3 text-md-end">€ 4.50</div>
                 </div>
-                <div class="row">
-                    <div class="col-9">7. Hamburger provola di bufala e patatine fritte</div>
+                <div class="row pb-3">
+                    <div class="col-9">7 - Hamburger provola di bufala e patatine fritte</div>
                     <div class="col-3 text-md-end">€ 5.50</div>
                 </div>
-                <div class="row">
-                    <div class="col-9">8. Hamburger stracciatella bacon e pesto di pistacchio</div>
+                <div class="row pb-3">
+                    <div class="col-9">8 - Hamburger stracciatella bacon e pesto di pistacchio</div>
                     <div class="col-3 text-md-end">€ 6.00</div>
                 </div>
-                <div class="row">
-                    <div class="col-9">9. Hamburger mortadella alla brace pesto di pistacchio e ciliegine di mozzarella</div>
+                <div class="row pb-3">
+                    <div class="col-9">9 - Hamburger mortadella alla brace pesto di pistacchio e ciliegine di mozzarella</div>
                     <div class="col-3 text-md-end">€ 6.50</div>
                 </div>
-                <div class="row">
-                    <div class="col-9">10. Hamburger maionese al pepe nero bacon e mozzarella</div>
+                <div class="row pb-3">
+                    <div class="col-9">10 - Hamburger maionese al pepe nero bacon e mozzarella</div>
                     <div class="col-3 text-md-end">€ 5.50</div>
                 </div>
-                <div class="row">
-                    <div class="col-9">11. Hamburger brie pomodori secchi prosciutto crudo e rucola</div>
+                <div class="row pb-3">
+                    <div class="col-9">11 - Hamburger brie pomodori secchi prosciutto crudo e rucola</div>
                     <div class="col-3 text-md-end">€ 7.00</div>
                 </div>
-                <div class="row">
-                    <div class="col-9">12. Hamburger bacon melanzane dorate salsa cheddar e patatine fritte</div>
+                <div class="row pb-3">
+                    <div class="col-9">12 - Hamburger bacon melanzane dorate salsa cheddar e patatine fritte</div>
                     <div class="col-3 text-md-end">€ 6.50</div>
                 </div>
-                <div class="row">
-                    <div class="col-9">13. Polpette al sugo e provola</div>
+                <div class="row pb-3">
+                    <div class="col-9">13 - Polpette al sugo e provola</div>
                     <div class="col-3 text-md-end">€ 5.50</div>
                 </div>
             </div>
