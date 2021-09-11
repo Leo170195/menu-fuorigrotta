e532fc38102fc96ec9782e8bc6f959eaba1fbdb7
Leonardo <decandia.leonardo95@gmail.com>
2021-09-11 12:54:13 +0200

    bevande

--- src/components/Body.vue
+++ src/components/Body.vue
@@ -7,7 +7,7 @@
             </button>
             <div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
             <div class="offcanvas-header bg-nav text-white">
-                <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Fuorigrotta</h5>
+                <h5 class="offcanvas-t pb-2itle" id="offcanvasNavbarLabel">Fuorigrotta</h5>
                 <button type="button" class="btn-close text-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
             </div>
             <div class="offcanvas-body bg-nav show">
@@ -53,6 +53,30 @@
                         <a class="nav-link text-decoration-none text-light" href="#panini" v-on:click="closeOffCanvas">
                             <img src="/img/icon/hamburger.png" alt="" height="30">
                             Panini
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-decoration-none text-light" href="#coke" v-on:click="closeOffCanvas">
+                            <img src="/img/icon/coke.png" alt="" height="30">
+                            Bevande
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-decoration-none text-light" href="#beers" v-on:click="closeOffCanvas">
+                            <img src="/img/icon/beers.png" alt="" height="30">
+                            Birre alla spina
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-decoration-none text-light" href="#beerb" v-on:click="closeOffCanvas">
+                            <img src="/img/icon/beerb.png" alt="" height="30">
+                            Birre in bottiglia
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-decoration-none text-light" href="#wine" v-on:click="closeOffCanvas">
+                            <img src="/img/icon/wine.png" alt="" height="30">
+                            Vini
                         </a>
                     </li>
                 </ul>
@@ -173,7 +197,7 @@
         <div class="container pb-5">
             <div class="row">
                 <div class="col-12">
-                    <h2 class="mb-3 text-center">Insalata <img src="/img/icon/salad.png" alt="" height="50" class="ps-3"></h2>
+                    <h2 class="mb-3 text-center">Insalate <img src="/img/icon/salad.png" alt="" height="50" class="ps-3"></h2>
                     <div class="row py-3">
                         <div class="col-9">Insalata tricolore (insalata iceberg, cubetti di mozzarella e julienne di pomodori)</div>
                         <div class="col-3 text-md-end">€ 6.00</div>
@@ -338,14 +362,210 @@
                         <div class="col-9">12 - Hamburger bacon melanzane dorate salsa cheddar e patatine fritte</div>
                         <div class="col-3 text-md-end">€ 6.50</div>
                     </div>
-                    <div class="row pb-3">
+                    <div id="coke" class="row pb-3">
                         <div class="col-9">13 - Polpette al sugo e provola</div>
                         <div class="col-3 text-md-end">€ 5.50</div>
                     </div>
                 </div>
             </div>
         </div>
-
+        <div class="container pb-5">
+            <div class="row">
+                <div class="col-12">
+                    <h2 class="mb-3 text-center">Bevande<img src="/img/icon/coke.png" alt="" height="50" class="ps-3"></h2>
+                    <div class="row py-3">
+                        <div class="col-9">Acqua minerale/naturale 50 cl</div>
+                        <div class="col-3 text-md-end">€ 1.00</div>
+                    </div>
+                    <div class="row pb-3">
+                        <div class="col-9">Acqua minerale/naturale 1 lt</div>
+                        <div class="col-3 text-md-end">€ 1.50</div>
+                    </div>
+                    <div class="row pb-3">
+                        <div class="col-9">Coca-cola/Coca-cola zero 33cl</div>
+                        <div class="col-3 text-md-end">€ 1.50</div>
+                    </div>
+                    <div class="row pb-3">
+                        <div class="col-9">Fanta 33cl</div>
+                        <div class="col-3 text-md-end">€ 1.50</div>
+                    </div>
+                    <div id="beers" class="row pb-3">
+                        <div class="col-9">Estathè pesca o limone 33cl</div>
+                        <div class="col-3 text-md-end">€ 2.00</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="container pb-5">
+            <div class="row">
+                <div class="col-12">
+                    <h2 class="mb-3 text-center">Birre alla spina<img src="/img/icon/beers.png" alt="" height="50" class="ps-3"></h2>
+                    <div class="row pt-3 pb-1">
+                        <div class="col-9">Stella Artois 20 cl.</div>
+                        <div class="col-3 text-md-end">€ 2.50</div>
+                    </div>
+                    <div class="row pb-3">
+                        <div class="col-9">Stella Artois 40 cl.</div>
+                        <div class="col-3 text-md-end">€ 4.50</div>
+                    </div>
+                    <div class="row pb-1">
+                        <div class="col-9">Leffe Rousse 25 cl.</div>
+                        <div class="col-3 text-md-end">€ 3.00</div>
+                    </div>
+                    <div class="row pb-3">
+                        <div class="col-9">Leffe Rousse 50 cl.</div>
+                        <div class="col-3 text-md-end">€ 5.00</div>
+                    </div>
+                    <div class="row pb-1">
+                        <div class="col-9">Leffe Blonde 25 cl.</div>
+                        <div class="col-3 text-md-end">€ 3.00</div>
+                    </div>
+                    <div id="beerb" class="row pb-3">
+                        <div class="col-9">Leffe Blonde 50 cl.</div>
+                        <div class="col-3 text-md-end">€ 5.00</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="container pb-5">
+            <div class="row">
+                <div class="col-12">
+                    <h2 class="mb-3 text-center">Birre in bottiglia<img src="/img/icon/beerb.png" alt="" height="50" class="ps-3"></h2>
+                    <div class="row py-3">
+                        <div class="col-9">Super 8 Export</div>
+                        <div class="col-3 text-md-end">€ 3.00</div>
+                    </div>
+                    <div class="row pb-3">
+                        <div class="col-9">Landbier Original</div>
+                        <div class="col-3 text-md-end">€ 4.50</div>
+                    </div>
+                    <div class="row pb-3">
+                        <div class="col-9">Bjorn</div>
+                        <div class="col-3 text-md-end">€ 3.00</div>
+                    </div>
+                    <div class="row pb-3">
+                        <div class="col-9">Grinbergen Blanche</div>
+                        <div class="col-3 text-md-end">€ 3.50</div>
+                    </div>
+                    <div class="row pb-3">
+                        <div class="col-9">Grinbergen Double</div>
+                        <div class="col-3 text-md-end">€ 3.50</div>
+                    </div>
+                    <div class="row pb-3">
+                        <div class="col-9">Vitus Weihenstephaner</div>
+                        <div class="col-3 text-md-end">€ 5.50</div>
+                    </div>
+                    <div class="row pb-3">
+                        <div class="col-9">Re Ale</div>
+                        <div class="col-3 text-md-end">€ 5.50</div>
+                    </div>
+                    <div class="row pb-3">
+                        <div class="col-9">Nazionale Birra Italiana</div>
+                        <div class="col-3 text-md-end">€ 5.00</div>
+                    </div>
+                    <div class="row pb-3">
+                        <div class="col-9">Chimay Premiere</div>
+                        <div class="col-3 text-md-end">€ 5.00</div>
+                    </div>
+                    <div class="row pb-3">
+                        <div class="col-9">La Trappe Triple</div>
+                        <div class="col-3 text-md-end">€ 5.50</div>
+                    </div>
+                    <div class="row pb-3">
+                        <div class="col-9">Gulden Draak</div>
+                        <div class="col-3 text-md-end">€ 5.50</div>
+                    </div>
+                    <div class="row pb-3">
+                        <div class="col-9">Monk's Stout Dupont</div>
+                        <div class="col-3 text-md-end">€ 4.00</div>
+                    </div>
+                    <div id="wine" class="row pb-3">
+                        <div class="col-9">Nora</div>
+                        <div class="col-3 text-md-end">€ 5.50</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="container pb-5">
+            <div class="row">
+                <div class="col-12">
+                    <h2 class="mb-3 text-center">Vini<img src="/img/icon/wine.png" alt="" height="50" class="ps-3"></h2>
+                    <h5 class="text-center pb-2">Vini Bianchi</h5>
+                    <div class="row pb-3">
+                        <div class="col-9">Pecorino</div>
+                        <div class="col-3 text-md-end">€ 20.00</div>
+                    </div>
+                    <div class="row pb-3">
+                        <div class="col-9">Pyrros</div>
+                        <div class="col-3 text-md-end">€ 18.00</div>
+                    </div>
+                    <div class="row pb-3">
+                        <div class="col-9">greco</div>
+                        <div class="col-3 text-md-end">€ 13.00</div>
+                    </div>
+                    <div class="row pb-3">
+                        <div class="col-9">Don Franc</div>
+                        <div class="col-3 text-md-end">€ 15.00</div>
+                    </div>
+                    <div class="row pb-3">
+                        <div class="col-9">Lacryma Christi</div>
+                        <div class="col-3 text-md-end">€ 20.00</div>
+                    </div>
+                    <h5 class="text-center pb-2">Vini Rosati</h5>
+                    <div class="row pb-3">
+                        <div class="col-9">Merlot</div>
+                        <div class="col-3 text-md-end">€ 20.00</div>
+                    </div>
+                    <div class="row pb-3">
+                        <div class="col-9">Philomena</div>
+                        <div class="col-3 text-md-end">€ 15.00</div>
+                    </div>
+                    <div class="row pb-3">
+                        <div class="col-9">Versura</div>
+                        <div class="col-3 text-md-end">€ 15.00</div>
+                    </div>
+                    <div class="row pb-3">
+                        <div class="col-9">Rondirosa</div>
+                        <div class="col-3 text-md-end">€ 13.00</div>
+                    </div>
+                    <div class="row pb-3">
+                        <div class="col-9">Lacryma Christi</div>
+                        <div class="col-3 text-md-end">€ 20.00</div>
+                    </div>
+                    <h5 class="text-center pb-2">Vini Rossi</h5>
+                    <div class="row pb-3">
+                        <div class="col-9">Primitivo magno</div>
+                        <div class="col-3 text-md-end">€ 18.00</div>
+                    </div>
+                    <div class="row pb-3">
+                        <div class="col-9">Padre</div>
+                        <div class="col-3 text-md-end">€ 18.00</div>
+                    </div>
+                    <div class="row pb-3">
+                        <div class="col-9">Gragnano</div>
+                        <div class="col-3 text-md-end">€ 20.00</div>
+                    </div>
+                    <div class="row pb-3">
+                        <div class="col-9">Lacryma Christi</div>
+                        <div class="col-3 text-md-end">€ 20.00</div>
+                    </div>
+                    <h5 class="text-center pb-2">Le Bollicine</h5>
+                    <div class="row pb-3">
+                        <div class="col-9">Curvèe Oro (extra dry)</div>
+                        <div class="col-3 text-md-end">€ 15.00</div>
+                    </div>
+                    <div class="row pb-3">
+                        <div class="col-9">Fantini Gran Curvèe Rosè</div>
+                        <div class="col-3 text-md-end">€ 22.00</div>
+                    </div>
+                    <div class="row pb-3">
+                        <div class="col-9">Fantini Gran Curvèe Bianco</div>
+                        <div class="col-3 text-md-end">€ 22.00</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
     </div>
 </template>
 
